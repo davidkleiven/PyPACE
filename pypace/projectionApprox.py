82697ac83a113e52e54f6affc78c570f321e15fa
David Kleiven <davidkleiven446@gmail.com>
2017-06-20 18:39:23 +0200

    Started to create a class that build a 3D array from 2D rotated projections

--- pypace/projectionApprox.py
+++ pypace/projectionApprox.py
@@ -1,6 +1,21 @@
 import numpy as np
 import copy
 from scipy.ndimage import interpolation as sciinterp
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+class MatrixBuilder3D:
+    """
+    This class builds a 3D matrix from rotated projections
+    It assumes that the data is rotated around the x-axis (axis=0)
+    It uses linear interpolation between two neighbouring slices to fill
+    one wedge at the time
+    """
+    def __init__( self, dim ):
+        self.data = np.zeros((dim,dim,dim))
+
+    #def insert( slice1, angle1Deg, slice2, stepAngleDeg ):
+
 
 class ProjectionPropagator(object):
     def __init__( self, deltaDistribution, wavelength, voxelsize, kspaceDim=512, maxDeltaValue=5E-5 ):
@@ -10,58 +25,39 @@ class ProjectionPropagator(object):
         self.kspaceDim=kspaceDim
 
     def yAxisIsMainAxis( self, angle ):
-        return angle < 45 or angle > 135
+        return angle < np.pi/4.0 or angle > 3.0*np.pi/4.0
+
+    def plotKspace( self, values ):
+        centerX = int(values.shape[0]/2)
+        plt.imshow( values[centerX,:,:], cmap="nipy_spectral", norm=mpl.colors.LogNorm(), interpolation="none")
+        plt.show()
 
     def generateKspace( self, angleStepDeg ):
-        angles = np.linspace( 0, 45, int( 45/angleStepDeg ) )
+        angles = np.linspace( 0, 180, int( 180/angleStepDeg ) )
         angles *= np.pi/180.0
+
+        # Initialize the new kspace matrix
         kspace = np.zeros((self.kspaceDim,self.kspaceDim,self.kspaceDim))
+
+        # Compute the wavenumber
         k = 2.0*np.pi/self.wavelength
+
+        # Compue the projection along the propagation direction
         proj0 = self.delta.sum(axis=2)*self.voxelsize
+
+        # The far field is given by the Fourier Transform of the exit field
         ff0 = np.abs( np.fft.fft2( np.exp(1j*k*proj0)-1.0, s=(self.kspaceDim,self.kspaceDim) ) )**2
         ff0 = np.fft.fftshift(ff0)
         for i in range( 0, len(angles)-1 ):
-            print (angles[i])
+            print (angles[i]*180.0/np.pi,angles[i+1]*180.0/np.pi)
+
+            # Rotate the material around the x-axis
             self.delta = sciinterp.rotate(self.delta, angleStepDeg, axes=(1,0), reshape=False )
+
+            # Compute the new projection
             proj1 = self.delta.sum(axis=2)*self.voxelsize
+
+            # Far field is given by the new projection
             ff1 = np.abs( np.fft.fft2( np.exp(1j*k*proj1)-1.0, s=(self.kspaceDim,self.kspaceDim) ) )**2
             ff1 = np.fft.fftshift(ff1)
-            for imain in range( int(self.kspaceDim/2) ):
-                if ( self.yAxisIsMainAxis(angles[i]) ):
-                    secondmin = int( -imain*np.sin(angles[i+1]) )
-                    secondmax = int( -imain*np.sin(angles[i]) )
-                else:
-                    secondmin = int( -imain*np.sin(np.pi/2.0-angles[i]) )
-                    secondmax = int( -imain*np.sin(np.pi/2.0-angles[i+1]) )
-
-                for isecond in range(secondmin,secondmax):
-                    weight = (isecond-secondmin)/(secondmax-secondmin)
-                    secondIndx = int( self.kspaceDim/2+isecond )
-                    mainIndx = int( self.kspaceDim/2+imain)
-                    indx = int( np.sqrt( isecond**2 + imain**2) + self.kspaceDim/2 )
-                    if ( indx >= 512 ):
-                        break
-                    if ( self.yAxisIsMainAxis(angles[i]) ):
-                        kspace[:,mainIndx,secondIndx] = ff1[:,indx]*weight + (1.0-weight)*ff0[:,indx]
-                    else:
-                        kspace[:,secondIndx,mainIndx] = ff1[:,indx]*weight + (1.0-weight)*ff0[:,indx]
-                if ( self.yAxisIsMainAxis(angles[i]) ):
-                    secondmin = int( imain*np.sin(angles[i]) )
-                    secondmax = int( imain*np.sin(angles[i+1]))
-                else:
-                    secondmin = int( imain*np.sin(np.pi/2.0-angles[i+1]) )
-                    secondmax = int( imain*np.sin(np.pi/2.0-angles[i]))
-
-                for isecond in range(secondmin,secondmax):
-                    weight = (isecond-secondmin)/(secondmax-secondmin)
-                    secondIndx = int( self.kspaceDim/2+isecond )
-                    mainIndx = int( self.kspaceDim/2-imain)
-                    indx = int( self.kspaceDim/2 - np.sqrt( isecond**2 + imain**2) )
-                    if ( indx < 0 ):
-                        break
-                    if ( self.yAxisIsMainAxis(angles[i]) ):
-                        kspace[:,mainIndx,secondIndx] = ff1[:,indx]*weight + (1.0-weight)*ff0[:,indx]
-                    else:
-                        kspace[:,secondIndx,mainIndx] = ff1[:,indx]*weight + (1.0-weight)*ff0[:,indx]
-            ff0[:,:] = ff1[:,:]
         return kspace
