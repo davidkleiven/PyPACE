4c3e799916ab44d5ae206dadcfe6f65fc7ce8fe9
David Kleiven <davidkleiven446@gmail.com>
2017-07-03 09:02:25 +0200

    Minor change to the submission script

--- submit.sh
+++ submit.sh
@@ -5,7 +5,7 @@ PyPath="/home/davidkl/Documents/PyPACE:/home/davidkl/Documents/PyPACE/pyREC"
 
 cluster=davidkl@neutron.phys.ntnu.no
 folder=/home/davidkl/Documents/PyPACE
-#scp ${data} ${cluster}:${folder}/pyREC/
 scp ${script} ${cluster}:${folder}/example/
+scp ${data} ${cluster}:${folder}/pyREC/
 ssh ${cluster} "PYTHONPATH=${PyPath}; export PYTHONPATH; LD_LIBRARY_PATH=/home/davidkl/privateLib/lib; export LD_LIBRARY_PATH;
 nice -19 python ${folder}/${script} ${folder}/${data} > output.txt"
