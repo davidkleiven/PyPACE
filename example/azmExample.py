06f87605e28b12ca385adabf98f0ddd980df6386
David Kleiven <davidkleiven446@gmail.com>
2017-07-06 08:52:12 +0200

    Reading number of iterations from the command line

--- example/azmExample.py
+++ example/azmExample.py
@@ -6,7 +6,12 @@ import azmDensityCorrector as adc
 from matplotlib import pyplot as plt
 from mpi4py import MPI
 
-def main():
+def main( argv ):
+    nIter = 12
+    for arg in argv:
+        if ( arg.find("--iter=") != -1 ):
+            nIter = int( arg.split("--iter=")[1] )
+
     reconstruct = "data/average_NiAu_sample1_3D_50_1.npy"
     kspace = "data/NiAu_sample1_3D.npy"
     comm = MPI.COMM_WORLD
@@ -19,10 +24,10 @@ def main():
     dCorr.plotMask()
     #dCorr.saveAllSliceClusters()
     print ("Optimizing parameters")
-    dCorr.fit( nIter=1, nClusters=6, maxDelta=1E-4 )
+    dCorr.fit( nIter=nIter, nClusters=6, maxDelta=1E-4 )
     dCorr.merge()
     #dCorr.plotFit( optimum["x"] )
     #plt.show()
 
 if __name__ == "__main__":
-    main()
+    main( sys.argv[1:] )
