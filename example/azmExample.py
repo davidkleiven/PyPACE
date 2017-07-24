36512c061300ac2124da01f2d66870147dbfab6d
David Kleiven <davidkleiven446@gmail.com>
2017-07-24 12:47:31 +0200

    Ready for sever

--- example/azmExample.py
+++ example/azmExample.py
@@ -40,8 +40,8 @@ def main( argv ):
     #dCorr.saveAllSliceClusters()
     print ("Optimizing parameters")
     width = int( dCorr.kspace.shape[0]/params["fractionCenterWidth"] )
-    #dCorr.fit( nIter=nIter, nClusters=params["nClusters"], maxDelta=1E-4, useSeparateClusterAtCenter=True, centerClusterWidth=width )
-    #dCorr.merge()
+    dCorr.fit( nIter=nIter, nClusters=params["nClusters"], maxDelta=1E-4, useSeparateClusterAtCenter=True, centerClusterWidth=width )
+    dCorr.merge()
     #plt.show()
 
     if ( haveMayavi ):
