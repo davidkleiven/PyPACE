bb74d9cba0916c795adea55d335964c70221ce97
David Kleiven <davidkleiven446@gmail.com>
2017-07-26 16:57:52 +0200

    Try to include custom sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -113,7 +113,8 @@ html_sidebars = {
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
         'donate.html',
-    ]
+        "sidebar.html",
+    ],
 }
 
 
