69be62a3ff5d3c348b45c8d692050acfed87609c
David Kleiven <davidkleiven446@gmail.com>
2017-07-26 11:45:43 +0200

    Cosmetic change

--- setup.py
+++ setup.py
@@ -2,26 +2,16 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 module1 = Extension( "categorize", sources=["C/categorize.c"], include_dirs=["C"] )
+
 module2 = Extension( "pypaceCython", ["pypace/pypaceCythonMP.pyx"], libraries=["m"], extra_compile_args=["-O3", "-ffast-math", "-march=native","-fopenmp"],
 extra_link_args=["-fopenmp"])
-#module3 = Extension( "shellCategorize", sources=["C/shellKmeans.cpp", "C/shellPartitioner.cpp", "C/shellCategorize.cpp"],
-#language="c++", libraries=["m"], extra_compile_args=["-O3", "-fPIC", "-ffast-math", "-march=native","-fopenmp"],
-#extra_link_args=["-fopenmp", "-std=c++11"])
 
-"""
-module4 = Extension( "constrainedpowerc", sources=["C/constrainedPowerOperator.cpp", "C/hermiteOperatorMatrix.cpp",
-"C/trilinearInterpolator.cpp", "C/constrainedOperator.cpp"], include_dirs=["C"], language="c++",
-extra_compile_args=["-fPIC", "-ffast-math", "-march=native","-fopenmp","-std=c++11"],
-extra_link_args=["-fopenmp", "-std=c++11"]
-)
-"""
-
-module5 = Extension("missingdatac", sources=["C/missingData.c"], include_dirs=["C"],
+module3 = Extension("missingdatac", sources=["C/missingData.c"], include_dirs=["C"],
 extra_compile_args=["-O3", "-ffast-math", "-march=native","-fopenmp"],
 extra_link_args=["-fopenmp", "-std=c++11"])
 
 setup(
     name = "pypace",
     cmdclass={"build_ext":build_ext},
-    ext_modules=[module1,module2,module5]
+    ext_modules=[module1,module2,module3]
 )
